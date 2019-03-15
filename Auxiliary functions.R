7181990382eb2828e03d2201daa266d623bb0dc0
Alice Lépissier <alice.lepissier@gmail.com>
2019-03-15 12:06:48 -0700

    Helper functions to plot evolution

---
+++ Auxiliary functions.R
@@ -0,0 +1,77 @@
+plot.pop.evol <- function(result, filter = F, gen.sequence = NULL){
+  pop.size <- result$pop.size
+  pop.evolution <- as.data.frame(result$pop.evolution)
+  pop.evolution <- gather(pop.evolution,
+                          Gen, Individual)
+  pop.evolution$Generation <- sapply(strsplit(pop.evolution$Gen, "_"), "[", 1)
+  pop.evolution$Variable <- sapply(strsplit(pop.evolution$Gen, "_"), "[", 2)
+  pop.evolution <- pop.evolution %>%
+    select(Generation, Variable, Individual)
+  
+  if(filter == TRUE){
+    plot.data <- pop.evolution %>%
+      filter(Generation %in% gen.sequence)
+  } else {
+    plot.data <- pop.evolution
+  }
+  
+  iter.num <- length(unique(plot.data$Generation))
+  nvars <- length(unique(plot.data$Variable))
+  
+  if (nvars > 1){
+    V1 <- plot.data %>%
+      filter(Variable == "V1") %>%
+      rename(Var1 = Individual) %>%
+      select(-Variable)
+    V2 <- plot.data %>%
+      filter(Variable == "V2") %>%
+      rename(Var2 = Individual) %>%
+      select(-Variable)
+    plot.data <- cbind(V1, V2 %>% select(Var2))
+  }
+  
+  if (nvars == 1){
+    g <- ggplot(plot.data,
+                aes(x = 1:(pop.size*iter.num),
+                    y = Individual,
+                    col = Generation))
+  } else {
+    g <- ggplot(plot.data,
+                aes(x = Var1, y = Var2, col = Generation))
+  }
+  g +
+    geom_point() +
+    labs(y = "Solution",
+         title = "Evolution of solution in population") +
+    scale_color_discrete(breaks = sort(as.numeric(plot.data$Generation))) +
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank())
+}
+
+plot.fitness.evol <- function(result, filter = F, gen.sequence = NULL){
+  pop.size <- result$pop.size
+  fitness.evolution <- as.data.frame(result$fitness.evolution)
+  fitness.evolution <- gather(fitness.evolution,
+                              Generation, Individual)
+  
+  if(filter == TRUE){
+    plot.data <- fitness.evolution %>%
+      filter(Generation %in% gen.sequence)
+  } else {
+    plot.data <- fitness.evolution
+  }
+  
+  iter.num <- length(unique(plot.data$Generation))
+  
+  ggplot(plot.data, 
+         aes(x = 1:(pop.size*iter.num), y = Individual, 
+             col = Generation)) +
+    geom_point() +
+    labs(y = "Fitness",
+         title = "Evolution of fitness in population") +
+    scale_color_discrete(breaks = sort(as.numeric(plot.data$Generation))) +
+    theme(axis.title.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.x = element_blank())
+}
